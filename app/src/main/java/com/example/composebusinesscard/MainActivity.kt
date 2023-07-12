c6077239c042eb009feb0e2576d849afdde5ff44
BlessedOneKobo <59095867+BlessedOneKobo@users.noreply.github.com>
2023-07-12 22:47:27 +0100

    feat: create ContactInformation

--- app/src/main/java/com/example/composebusinesscard/MainActivity.kt
+++ app/src/main/java/com/example/composebusinesscard/MainActivity.kt
@@ -3,13 +3,25 @@ package com.example.composebusinesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composebusinesscard.ui.theme.ComposeBusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +34,6 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
                 }
             }
         }
@@ -30,17 +41,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun ContactInformation(icon: ImageVector, text: String, modifier: Modifier = Modifier) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null,
+            tint = Color(0xFF3ddc84),
+        )
+        Spacer(modifier.size(12.dp))
+        Text(
+            text = text
+        )
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BusinessCardPreview() {
     ComposeBusinessCardTheme {
-        Greeting("Android")
+        ContactInformation(icon = Icons.Default.Phone, text = "+00 (00) 000 000")
     }
 }
